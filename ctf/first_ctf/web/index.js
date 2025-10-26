78a6637de8aefd66cab753fb19f999af2b25e5c7
sammy-ulfh <sammy_ulfh@proton.me>
2025-10-26 14:43:58 -0600

    Details fixed

--- ctf/first_ctf/web/index.js
+++ ctf/first_ctf/web/index.js
@@ -3,12 +3,14 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const app = express();
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
 	session({ secret: "secret123", resave: false, saveUninitialized: true })
 );
+app.use(express.json());
 
-const creds = { Administrator: "twinkle", john: "1122112233" };
+const creds = { Administrator: "twinkle", john: "1122112233", guest: "123456" };
 
 app.get("/", (_, res) => res.sendFile(__dirname + "/index.html"));
 
@@ -17,13 +19,27 @@ app.post("/login", (req, res) => {
 	if (creds[username] === password) {
 		req.session.user = username;
 
-		if (username === "john") {
+		if (username === "john" || username === "guest") {
 			return res.redirect("/dashboard");
 		} else if (username === "Administrator") {
 			return res.redirect("/admin");
 		}
 	}
 	return res.send("Invalid username or password");
+});
+
+app.post("/validation", (req, res) => {
+  const { flag } = req.body || {};
+
+  if ( flag === "PWG{MY_FIRST_TRY}" ){
+    return res.json({ result: 1, username: "bruteforce", silence: "firee" });
+  }else if ( flag === "PWG{IM_HACKING}" ){
+    return res.json({ result: 2 });
+  }else {
+    return res.json({ result: 0 });
+  }
+
+	return res.send("Invalid flag");
 });
 
 app.get("/dashboard", (req, res) => {
@@ -40,4 +56,4 @@ app.get("/admin", (req, res) => {
 	return res.status(403).send("Forbidden");
 });
 
-app.listen(80, "0.0.0.0", () => console.log("listening on 8080"));
+app.listen(80, "0.0.0.0", () => console.log("listening on 80"));
